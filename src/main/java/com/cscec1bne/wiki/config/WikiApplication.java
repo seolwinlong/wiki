42063450cc2a3815ea9239ac857989dc7c0c4115
winlong <winlong@outlook.com>
2022-12-29 18:07:18 +0800

    2-5 开发Hello World接口 访问地址：http://localhost/hello

--- src/main/java/com/cscec1bne/wiki/config/WikiApplication.java
+++ src/main/java/com/cscec1bne/wiki/config/WikiApplication.java
@@ -1,8 +1,10 @@
-package com.cscec1bne.wiki;
+package com.cscec1bne.wiki.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan("com.cscec1bne")
 @SpringBootApplication
 public class WikiApplication {
 
