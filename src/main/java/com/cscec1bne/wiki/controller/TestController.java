66b9cdd3228436e2530be1cfee5862d891854e6a
winlong <winlong@outlook.com>
2022-12-30 09:38:45 +0800

    2-6 增加hello接口测试test.http

--- src/main/java/com/cscec1bne/wiki/controller/TestController.java
+++ src/main/java/com/cscec1bne/wiki/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.cscec1bne.wiki.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,5 +9,10 @@ public class TestController {
     @RequestMapping("/hello")
     public String hello(){
         return "Hello world";
+    }
+
+    @PostMapping("/hello/post")
+    public String helloPost(String name){
+        return "Hello world,"+name;
     }
 }
