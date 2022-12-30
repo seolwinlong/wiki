afbfd800c9a0e236397ea0dcc4152842336ea594
winlong <winlong@outlook.com>
2022-12-30 12:21:08 +0800

    2-8 增加自定义配置项并读取配置项

--- src/main/java/com/cscec1bne/wiki/controller/TestController.java
+++ src/main/java/com/cscec1bne/wiki/controller/TestController.java
@@ -1,14 +1,18 @@
 package com.cscec1bne.wiki.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
+    @Value("${test.hello:TEST}")
+    private String test;
+
     @RequestMapping("/hello")
     public String hello(){
-        return "Hello world";
+        return "Hello world"+test;
     }
 
     @PostMapping("/hello/post")
